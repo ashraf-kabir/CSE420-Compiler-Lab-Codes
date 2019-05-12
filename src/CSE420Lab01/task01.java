92ab600425d70db5987cd339b3c34213a89159f6
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-05-12 23:38:28 +0600

    Initial commit

---
+++ src/CSE420Lab01/task01.java
@@ -0,0 +1,21 @@
+package CSE420Lab01;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class task01 {
+    public static void main(String[] args) {
+
+        String scheck = "House 15, Road 2, Shyamoli, Dhaka 1207";
+        String regNum = "\\d+";
+
+        Pattern p = Pattern.compile(regNum);
+
+        Matcher m = p.matcher(scheck);
+
+        while (m.find()) {
+            System.out.println(m.group());
+        }
+
+    }
+}
